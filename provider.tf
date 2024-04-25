99ac3f2768c766409e4257df26d56494f3c4d2d7
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-25 14:43:34 +0100

    add object store

--- provider.tf
+++ provider.tf
@@ -1,4 +1,20 @@
 terraform {
+    backend "s3" {
+        bucket                      = "terraform-state"
+        key                         = "terraform.tfstate"
+        region                      = "LON1"
+        endpoints = {
+        s3 = "https://objectstore.lon1.civo.com"
+        }
+        skip_region_validation      = true
+        skip_credentials_validation = true
+        skip_metadata_api_check     = true
+        skip_requesting_account_id  = true
+        use_path_style              = true
+        access_key = "0UMMDZ9BBNS2E2SM018F"
+        secret_key = "HsUpLimzeIoSAwilYetUB6KnkrxG7ozarFWxFhMN"
+  }
+
   required_providers {
     civo = {
       source = "civo/civo"
@@ -9,4 +25,5 @@ terraform {
 
 provider "civo" {
   region = "LON1"
+  token = "qHbNSMLkiTe9DYGAItKpZVSCYKqRtGBCQLJlL691nmPkQ6Q3J7"
 }
