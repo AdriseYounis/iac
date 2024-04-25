1c9666735783a53d3ec362204c971bf7e9fe8947
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-25 15:35:56 +0100

    remove aws checks

--- provider.tf
+++ provider.tf
@@ -6,6 +6,9 @@ terraform {
         endpoints = {
         s3 = "https://objectstore.lon1.civo.com"
         }
+        skip_region_validation      = true
+        skip_metadata_api_check     = true
+        skip_requesting_account_id  = true
         skip_credentials_validation = true
         use_path_style              = true
   }
