30e505acb11efa684c36319ca39c1f1d520f9462
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-26 15:03:24 +0100

    update

--- object-store.tf
+++ object-store.tf
@@ -13,6 +13,6 @@ resource "civo_object_store" "tf_os_state" {
     name = "tf-os-state"
     max_size_gb = 500
     region = "LON1"
-    access_key_id = civo_object_store_credential.tf_state_creds.access_key_id
-    depends_on = [civo_object_store_credential.ds_tf_state_creds]
+    access_key_id = data.civo_object_store_credential.ds_tf_state_creds.access_key_id
+    depends_on = [data.civo_object_store_credential.ds_tf_state_creds]
 }
