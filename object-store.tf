b6d417fea6445ee9e706a69c9c49907ca8689fb9
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-26 14:56:19 +0100

    rename objects

--- object-store.tf
+++ object-store.tf
@@ -1,16 +1,16 @@
-resource "civo_object_store_credential" "tf-state-creds" {
+resource "civo_object_store_credential" "tf_state_creds" {
     name = "tf-os-creds"
     access_key_id = "adrise-access-key"
     secret_access_key = "adrise-secret-key"
 }
 
-data "civo_object_store_credential" "tf-state-creds" {
+data "civo_object_store_credential" "tf_state_creds" {
     name = "tf-os-creds"
     depends_on = [civo_object_store_credential.tf-state-creds]
 }
 
-resource "civo_object_store" "tf-os" {
-    name = "tf-state"
+resource "civo_object_store" "tf_os_state" {
+    name = "tf-os-state"
     max_size_gb = 500
     region = "LON1"
     access_key_id = civo_object_store_credential.tf-state-creds.access_key_id
