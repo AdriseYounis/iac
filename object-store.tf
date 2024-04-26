ae4a24e51b41bf8978249967db1ff790000e071e
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-26 14:27:28 +0100

    add depends on

--- object-store.tf
+++ object-store.tf
@@ -6,6 +6,7 @@ resource "civo_object_store_credential" "tf-state-creds" {
 
 data "civo_object_store_credential" "tf-state-creds" {
     name = "tf-os-creds"
+    depends_on = [civo_object_store_credential.tf-state-creds]
 }
 
 resource "civo_object_store" "tf-os" {
@@ -13,4 +14,5 @@ resource "civo_object_store" "tf-os" {
     max_size_gb = 500
     region = "LON1"
     access_key_id = civo_object_store_credential.tf-state-creds.access_key_id
+    depends_on = [civo_object_store_credential.tf-state-creds]
 }
