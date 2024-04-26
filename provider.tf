2cc4588b9dc997a995099305d553b9a83aedb73f
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-26 15:23:44 +0100

    pass keys to backend s3

--- provider.tf
+++ provider.tf
@@ -11,6 +11,8 @@ terraform {
         skip_requesting_account_id  = true
         skip_credentials_validation = true
         use_path_style              = true
+        access_key = var.access_key_id
+        secret_key = var.secret_key
   }
 
   required_providers {
