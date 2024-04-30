da15cfe602936a9f79ddd810d40a3cf734a08d7e
Adrise Younis <adriseyounis@Adrises-MacBook-Pro.local>
2024-04-30 11:36:49 +0100

    test new releases with helm

--- helm.tf
+++ helm.tf
@@ -1,18 +1,22 @@
 resource "helm_release" "prometheus" {
-  name       = "my-prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
-  version    = "15.0.0"
+  name             = "prom"
+  chart            = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  namespace        = "monitoring"
+  version          = "17.1.3"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
 }
 
 resource "helm_release" "nginx" {
-  name       = "my-nginx"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-  version    = "8.0.1"
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
+  name        = "nginx"
+  chart       = "nginx"
+  repository  = "./charts"
+  namespace   = "nginx"
+  max_history = 3
+  create_namespace = true
+  wait             = true
+  reset_values     = true
 }
